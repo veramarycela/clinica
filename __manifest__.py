3b426f161e0344c58df04494fe26cecb714d29a6
veramarycela <veramarycela@gmail.com>
2023-11-15 10:30:42 -0400

    primer commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Modulo Clinica",
+
+    'summary': """
+        Este modulo sirve para registrar operaciones de la clinica""",
+
+    'description': """
+        Este modulo sirve para registrar operaciones de la clinica
+    """,
+    'author': "ServiTecht",
+    'website': "http://www.servitecht.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'account'],
+    'data': [
+            'views/views.xml', 
+            'views/views_clinica_cliente_paciente.xml',
+            'security/ir.model.access.csv',
+            'report/factura_clinica_reporte.xml',
+            'template/factura_clinica_template.xml',
+    ],
+   
+}
